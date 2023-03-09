f4bb47651f3fc01d58446cd5188e5b68c3ebfcf8
Krish-Parekh <krishparekh303@gmail.com>
2023-03-09 19:40:30 +0530

    Incrementing Counter

---
+++ 15 - Incrementing Counter/script.js
@@ -0,0 +1,18 @@
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+    counter.innerText = '0';
+    const updateCounter = () => {
+        const target = parseInt(counter.getAttribute('data-target'));
+        const c = parseInt(counter.innerText);
+
+        const increment = target / 200;
+        if(c < target){
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 1);
+        }else{
+            counter.innerText = target;
+        }
+    }
+    updateCounter();
+})
